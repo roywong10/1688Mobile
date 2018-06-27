af26f4f5581acf60e256067f7fa0c1652c1d02d2
DESKTOP-SILQFBO\wangr <wangruirrrwww@gmail.com>
2018-06-27 10:34:59 +1000

    update files and dirs

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangr
+ * Date: 2018/6/27
+ * Time: 10:19
+ */
+?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>我的生活</title>
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="/favicon.ico">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css">
+<link rel="stylesheet" href="//g.alicdn.com/msui/sm/0.6.2/css/sm-extend.min.css">
+<link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="assets/js/scripts.js" charset='utf-8'></script>
